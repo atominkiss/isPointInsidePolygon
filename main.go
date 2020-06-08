9d796afff0ddc6fd500fcbd0a527f756340ba9ac
atomink <atomink@gmail.com>
2020-06-08 14:21:49 +0300

    ++ isPointInside work correct

--- main.go
+++ main.go
@@ -2,105 +2,72 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 	"unicode"
 )
 
-// входные данные: точка на карте и полигон. Необходимо выяснить: 1.Находится точка внутри полигона или нет.
+// входные данные: точка на карте и полигон.
+// Необходимо выяснить: 1.Находится точка внутри полигона или нет.
 // сначала идет долгота потом широта [46.066426,51.572922]
 
-func main(){
+func main() {
 	var polygon, point string
 	// var pointInside bool
 	var coordPolygonArrStr, coordPointArrStr []string
 	var longitudePolygon, latitudePolygon, coordPoint []float64 // долгота, широта
-	polygon = "[{\"type\":\"Feature\",\"properties\":{},\"geometry\":{\"type\":\"Polygon\",\"coordinates\":[[[46.066426,51.572922],[46.069762,51.573089],[46.069923,51.57208],[46.068872,51.571318],[46.066362,51.569258],[46.064828,51.569218],[46.064581,51.571003],[46.064581,51.571057],[46.066641,51.571184],[46.066426,51.572922]]]}}]"
-	point ="{\n  \"type\": \"FeatureCollection\",\n  \"features\": [\n    {\n      \"type\": \"Feature\",\n      \"properties\": {},\n      \"geometry\": {\n        \"type\": \"Point\",\n        \"coordinates\": [\n          43.44976902008056,\n          56.23616843459138\n        ]\n      }\n    }\n  ]\n}"
+	polygon = "{\n      \"type\": \"Feature\",\n      \"properties\": {},\n      \"geometry\": {\n        \"type\": \"Polygon\",\n        \"coordinates\": [\n          [\n            [\n              43.448342084884644,\n              56.23708370966285\n            ],\n            [\n              43.44818115234375,\n              56.23471050828792\n            ],\n            [\n              43.4518826007843,\n              56.23475224928091\n            ],\n            [\n              43.45214009284973,\n              56.23667228578949\n            ],\n            [\n              43.448342084884644,\n              56.23708370966285\n            ]\n          ]\n        ]\n      }"
+	point = "{\n  \"type\": \"FeatureCollection\",\n  \"features\": [\n    {\n      \"type\": \"Feature\",\n      \"properties\": {},\n      \"geometry\": {\n        \"type\": \"Point\",\n        \"coordinates\": [\n          43.45165729522705,\n          56.23481187919196\n        ]\n      }\n    }\n  ]\n}"
 	coordPolygonArrStr = extractCoordFromJsonToStrArr(polygon)
 	coordPointArrStr = extractCoordFromJsonToStrArr(point)
 	longitudePolygon, latitudePolygon = fromStrToFloatArr(coordPolygonArrStr)
 	coordPoint = fromStrToFloatArrPoint(coordPointArrStr)
-	
-	pointInside := isPointInside(latitudePolygon, longitudePolygon ,  coordPoint)
-	
-	// fmt.Println(longitudePolygon)
-	// fmt.Println(latitudePolygon)
-	// fmt.Println(coordPoint)
+
+	pointInside := isPointInside(latitudePolygon, longitudePolygon, coordPoint)
+
 	fmt.Println(pointInside)
-	
+
 }
 
 func isPointInside(x []float64, y []float64, coordPoint []float64) bool {
 	// многоугольник = 3 вершинам минимум. точку и отрезок игнорируем
 	// x - широта (latitude), y - долгота(longitude)
-	// quantity := len(x)
-	// if  quantity <= 2 {
-	// 	return false
-	// }
-	//
-	// intersectionsNum := 0
-	// prev := quantity - 1
-	// prev_under := y[prev] < coordPoint[2]
-	//
-	// for i := 0; i < quantity; i++ {
-	// 	cur_under := y[i] < coordPoint[2]
-	//
-	// 	a := polygon[prev] - point
-	// 	b := polygon[i]  - point
-	//
-	// 	t := (a.x*(b.y - a.y) - a.y*(b.x - a.x))
-	//
-	// 	if cur_under && !prev_under {
-	// 		if t > 0 {
-	// 			intersectionsNum += 1
-	// 		}
-	// 		if !cur_under && prev_under {
-	// 			if t < 0 {
-	// 				intersectionsNum += 1
-	// 			}
-	// 		}
-	// 	}
-	//
-	// 	prev = i
-	// 	prev_under = cur_under
-	// }
-	//
-	// return (intersectionsNum &1) != 0
-	
+
 	var i, j int
 	var c bool
 	npol := len(x)
-	for i := 0; i < npol; i++ {
-		// j = npol - 1  j = i++
-		if (((yp [i] <= y) && (y < yp [j])) || ((yp [j] <= y) && (y < yp [i]))) &&
-					(x < (xp [j] - xp [i]) * (y - yp [i]) / (yp [j] - yp [i]) + xp [i]) {
+	j = npol - 1
+	for i = 0; i < npol; i++ {
+		if (((y[i] <= coordPoint[0]) && (coordPoint[0] < y[j])) || ((y[j] <= coordPoint[0]) && (coordPoint[0] < y[i]))) &&
+			(coordPoint[1] < (x[j]-x[i])*(coordPoint[0]-y[i])/(y[j]-y[i])+x[i]) {
 			c = !c
 		}
-		
+		if i+2 >= npol {
+			j = i + 2 - npol
+		} else {
+			j = i + 2
+		}
 	}
-	
+
 	return c
-	
-	
+
 }
 
 func fromStrToFloatArrPoint(str []string) []float64 {
 	pointArr := make([]float64, len(str))
-	
+
 	for i := 0; i < len(str); i++ {
 		pointArr[i], _ = strconv.ParseFloat(str[i], 64)
 	}
-	
+
 	return pointArr
 }
 
 func fromStrToFloatArr(arrayString []string) ([]float64, []float64) {
 	longit := make([]float64, len(arrayString)/2)
-	latid:= make([]float64, len(arrayString)/2)
+	latid := make([]float64, len(arrayString)/2)
 	var lo, la int
-	
+
 	for i := 0; i < len(arrayString); i++ {
 		coordinate, _ := strconv.ParseFloat(arrayString[i], 64)
 		if i%2 == 0 {
@@ -111,15 +78,15 @@ func fromStrToFloatArr(arrayString []string) ([]float64, []float64) {
 			la++
 		}
 	}
-	sort.Float64s(longit)
-	sort.Float64s(latid)
-	
+	// sort.Float64s(longit)
+	// sort.Float64s(latid)
+
 	return longit, latid
 }
 
 func extractCoordFromJsonToStrArr(polygon string) []string {
-	for i :=0; i< len(polygon); i++{
-		if unicode.IsDigit(rune(polygon[i])) || rune(polygon[i]) == '.' || rune(polygon[i]) == ','  {
+	for i := 0; i < len(polygon); i++ {
+		if unicode.IsDigit(rune(polygon[i])) || rune(polygon[i]) == '.' || rune(polygon[i]) == ',' {
 			continue
 		} else {
 			polygon = strings.ReplaceAll(polygon, string(polygon[i]), "")
